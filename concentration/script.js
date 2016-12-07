cc86d83bc30c722bfbf80b793b9f33c74c3078bd
Anna Rogers <2muddybarefeet@gmail.com>
2016-12-07 13:18:09 -0800

    add reset logic

--- concentration/script.js
+++ concentration/script.js
@@ -64,4 +64,28 @@
 
   });
 
+  ractive.on('startAgain', function () {
+    //set the state back to its inital state
+    ractive.reset({
+      card: {
+        back: "http://vignette3.wikia.nocookie.net/youtubepoop/images/4/4c/Pokeball.png/revision/latest",
+        width: "100"
+      },
+      clickedKeypath: null,
+      pairsFlipped: 0,
+      busy: false,
+      win: false,
+      board: [
+              {image:"http://cdn.bulbagarden.net/upload/thumb/7/73/004Charmander.png/600px-004Charmander.png", clicked: false},
+              {image:"http://cdn.bulbagarden.net/upload/2/21/001Bulbasaur.png", clicked: false},
+              {image:"http://cdn.bulbagarden.net/upload/thumb/7/73/004Charmander.png/600px-004Charmander.png", clicked: false},
+              {image:"http://cdn.bulbagarden.net/upload/4/41/130Gyarados.png", clicked: false},
+              {image:"http://cdn.bulbagarden.net/upload/thumb/3/3e/039Jigglypuff.png/250px-039Jigglypuff.png", clicked: false},
+              {image:"http://cdn.bulbagarden.net/upload/thumb/3/3e/039Jigglypuff.png/250px-039Jigglypuff.png", clicked: false},
+              {image:"http://cdn.bulbagarden.net/upload/2/21/001Bulbasaur.png", clicked: false},
+              {image:"http://cdn.bulbagarden.net/upload/4/41/130Gyarados.png", clicked: false}
+            ]
+    });
+  });
+
 })();
