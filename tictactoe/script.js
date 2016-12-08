012f8055cf58efa6dbd4805a707bab66379c7f76
Anna Rogers <2muddybarefeet@gmail.com>
2016-12-08 14:12:46 -0800

    display board and on click show naught or cross

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -4,9 +4,27 @@
     el: "#container",
     template: "#template",
     data: {
-      greeting: "world"
+      nextToPlay: "O",
+      board: [
+        {cell1:"", cell2:"", cell3:""},
+        {cell1:"", cell2:"", cell3:""},
+        {cell1:"", cell2:"", cell3:""},
+      ]
     }
     
+  });
+
+  ractive.on('select', function (index, whichCell) {
+    //place the current piece and stop this square from being clicked again
+    var currentSelection = ractive.get('board['+index+']');
+    currentSelection[whichCell] = ractive.get('nextToPlay');
+    if (ractive.get('nextToPlay') === "O") {
+      ractive.set('nextToPlay', "X");
+    } else {
+      ractive.set('nextToPlay', "O");
+    }
+    ractive.update('board');
+
   });
 
 
